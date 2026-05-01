a2308bfd3ab7be8cbe7401cd171db141f5829250
flavio <flaviolopes1027@gmail.com>
2026-04-30 23:34:29 -0300

    fix: update migrations

---
+++ bunny-uploader/database/migrations/2026_04_29_181047_create_videos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('bunny_video_id')->nullable(); // garante que possa ser utilizado null
+            $table->foreignId('collection_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
