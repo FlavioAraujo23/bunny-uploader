66d76cfcd47e67117b8490f91de278c563487100
flavio <flaviolopes1027@gmail.com>
2026-05-04 17:54:06 -0300

    feat: implement a job for handle with create video process

--- bunny-uploader/app/Jobs/ProcessVideoUpload.php
+++ bunny-uploader/app/Jobs/ProcessVideoUpload.php
@@ -2,9 +2,19 @@
 
 namespace App\Jobs;
 
+use App\Models\Video;
+use App\Services\BunnyVideoService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\Attributes\Backoff;
+use Illuminate\Queue\Attributes\Timeout;
+use Illuminate\Queue\Attributes\Tries;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
+#[Backoff(60)]
+#[Tries(3)]
+#[Timeout(300)]
 class ProcessVideoUpload implements ShouldQueue
 {
     use Queueable;
@@ -12,16 +22,32 @@ class ProcessVideoUpload implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(private Video $video, private string $tempPath, private string $collection_id) {}
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(BunnyVideoService $service): void
     {
-        //
+        $this->video->update(['status' => Video::STATUS_PROCESSING]);
+
+        $content = file_get_contents($this->tempPath);
+
+        $data = $service->createVideo($this->video->name, $this->collection_id);
+        $service->uploadVideo($data['guid'], $content);
+
+        $this->video->update(['status' => Video::STATUS_DONE, 'bunny_video_id' => $data['guid']]);
+    }
+
+
+    /**
+     * Handle a job failure.
+     */
+    public function failed(?Throwable $exception): void
+    {
+        $this->video->update(['status' => Video::STATUS_FAILED]);
+        Log::error('Something went wrong while process an video.', [
+            'exception' => $exception->getMessage()
+        ]);
     }
 }
