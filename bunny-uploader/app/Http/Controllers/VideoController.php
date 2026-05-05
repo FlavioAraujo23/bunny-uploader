a1dc0610427f7fc5d7c341664ed862effeb97a59
flavio <flaviolopes1027@gmail.com>
2026-05-05 08:49:09 -0300

    feat: add controller to handle the logic for create and update videos

---
+++ bunny-uploader/app/Http/Controllers/VideoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\ProcessVideoUpload;
+use App\Models\Video;
+use App\Services\BunnyVideoService;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    public function __construct(private BunnyVideoService $service) {}
+
+    public function index()
+    {
+
+        $videos = Video::with('collection')->get();
+
+        return view('videos.index', ['videos' => $videos]);
+    }
+
+    public function store(Request $request)
+    {
+        $activeCollection = session('active_collection');
+        if (!isset($activeCollection)) {
+            return redirect()->route('videos.index')->with('Error', 'Nenhuma collection está ativa!');
+        }
+
+        if (!$request->hasFile('videos')) {
+            return redirect()->route('videos.index')->with('Error', 'Existe algo de errado com o video enviado.');
+        }
+
+        $data = $request->file('videos');
+
+        foreach ($data as $videoFile) {
+            $tempPath = $videoFile->store('temp');
+
+            $video = Video::create([
+                'collection_id' => $activeCollection,
+                'status' => Video::STATUS_PENDING,
+                'name' => $videoFile->getClientOriginalName()
+            ]);
+            
+            ProcessVideoUpload::dispatch($video, $tempPath, $activeCollection);
+        }
+
+        return redirect()->route('videos.index')->with('success', 'Videos enviados para o bunny!');
+    }
+
+    public function update(Request $request, Video $video)
+    {
+        $video->update(['name' => $request->input('name')]);
+        return redirect()->route('videos.index')->with('success', 'O nome do video foi atualizado com sucesso!');
+    }
+}
